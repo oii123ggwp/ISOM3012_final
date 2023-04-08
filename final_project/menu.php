46454989cf8b7227fee3d8e6e67de8d4fcd89f30
chenglammmm <125729277+chenglammmm@users.noreply.github.com>
2023-04-08 16:12:43 +0800

    Add files via upload

---
+++ final_project/menu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<body> 
+<link rel="stylesheet" type="text/css" href="menu.css">
+
+<div  style="text-align:center;">
+<br>
+<img src="image/logo.png" width=100px height=100px>
+</div>
+
+<div class="focus">
+  </div>
+</div>
+
+
