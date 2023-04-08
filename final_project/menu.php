ff3fecaf2cefac9fb70e85ff6d95602324a271c4
chenglammmm <125729277+chenglammmm@users.noreply.github.com>
2023-04-08 19:52:47 +0800

    Add files via upload

--- final_project/menu.php
+++ final_project/menu.php
@@ -2,13 +2,51 @@
 <body> 
 <link rel="stylesheet" type="text/css" href="menu.css">
 
+<div class="icon">
+  <a href=""><img src="image/icon3.png" title="message" ></a>
+  <a href=""><img src="image/icon2.png" title="shopping cart"></a>
+  <a href=""><img src="image/icon1.png" title="user"></a>
+  <a href=""><img src="image/icon4.png" title="login"></a>
+</div>
+
 <div  style="text-align:center;">
 <br>
-<img src="image/logo.png" width=100px height=100px>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<img src="image/logo.png" width=100px height=100px >
+</div>
+
+<div class="bar">
+  <ul class="bar1">
+    <li class="item">
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <a href="">Products</a>
+    </li>
+    <li class="item">
+      <a href="">Search</a>
+    </li>
+    <li class="item">
+      <a href="">FAQ</a>
+    </li>
+  </ul>
+<br>
 </div>
 
 <div class="focus">
   </div>
-</div>
+
+
 
 
