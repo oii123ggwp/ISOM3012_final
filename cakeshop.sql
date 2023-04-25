64a9a6c635e0c7a2327daa04e6724cb5a3f703cc
oii123ggwp <89623892+oii123ggwp@users.noreply.github.com>
2023-04-25 13:49:40 +0800

    Add files via upload

--- cakeshop.sql
+++ cakeshop.sql
@@ -2,9 +2,9 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- 主機： localhost
--- 產生時間： 2023 年 04 月 14 日 09:00
--- 伺服器版本： 10.4.18-MariaDB
+-- 主机： localhost
+-- 生成日期： 2023-04-25 07:49:23
+-- 服务器版本： 10.4.18-MariaDB
 -- PHP 版本： 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `cakeshop`
+-- 数据库： `cakeshop`
 --
 CREATE DATABASE IF NOT EXISTS `cakeshop` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `cakeshop`;
@@ -26,7 +26,7 @@ USE `cakeshop`;
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `feedback`
+-- 表的结构 `feedback`
 --
 
 CREATE TABLE `feedback` (
@@ -38,7 +38,7 @@ CREATE TABLE `feedback` (
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `product`
+-- 表的结构 `product`
 --
 
 CREATE TABLE `product` (
@@ -49,11 +49,12 @@ CREATE TABLE `product` (
   `product_description` varchar(1000) NOT NULL,
   `Type` varchar(100) NOT NULL,
   `quantity_in_stock` int(100) NOT NULL,
-  `being_click` int(11) NOT NULL
+  `being_click` int(11) NOT NULL,
+  `product_img_path2` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 傾印資料表的資料 `product`
+-- 转存表中的数据 `product`
 --
 
 INSERT INTO `product` (`product_id`, `product_name`, `product_price`, `product_img_path`, `product_description`, `Type`, `quantity_in_stock`, `being_click`, `product_img_path2`) VALUES
@@ -72,12 +73,11 @@ INSERT INTO `product` (`product_id`, `product_name`, `product_price`, `product_i
 ('P013', 'ROSE CAKE', '205', 'wedding1.jpg', 'A rose cake is a cake flavored with rose water and paired with a rose buttercream. It is fittingly decorated with cream flowers', 'wedding', 10, 0, 'wedding1-cake.jpg'),
 ('P014', 'ROYAL WEDDING CAKE', '200', 'wedding3.jpg', 'A rose cake is a cake flavored with rose water and paired with a rose buttercream. It is fittingly decorated with cream flowers', 'wedding', 10, 0, 'wedding3-cake.jpg'),
 ('P015', 'LAVENDER CAKE', '225', 'wedding2.jpg', 'A three-tier sponge cake, is a moist layer cake flavored with vanilla tea.', 'wedding', 10, 0, 'wedding2-cake.jpg');
-COMMIT;
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `transaction`
+-- 表的结构 `transaction`
 --
 
 CREATE TABLE `transaction` (
@@ -85,21 +85,30 @@ CREATE TABLE `transaction` (
   `user_id` varchar(20) NOT NULL,
   `product_id` varchar(20) NOT NULL,
   `payment_method` varchar(20) NOT NULL,
-  `total_payment_amount` decimal(20,0) NOT NULL,
-  `order_time` datetime(6) NOT NULL,
-  `finish_time` datetime(6) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `total_payment_amount` int(20) NOT NULL,
+  `order_time` date NOT NULL,
+  `finish_time` date NOT NULL,
   `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 转存表中的数据 `transaction`
+--
+
+INSERT INTO `transaction` (`transaction_id`, `user_id`, `product_id`, `payment_method`, `quantity`, `total_payment_amount`, `order_time`, `finish_time`, `status`) VALUES
+('T0', 'gG12345', 'P001', 'unset', 1, 240, '2023-04-24', '2023-04-04', 'NULL'),
+('T1', 'gG12345', 'P002', 'unset', 1, 220, '2023-04-24', '2023-04-04', 'NULL');
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `user`
+-- 表的结构 `user`
 --
 
 CREATE TABLE `user` (
   `user_id` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(20000) NOT NULL,
   `person_name` char(20) NOT NULL,
   `birthday` date NOT NULL,
   `gender` varchar(20) NOT NULL,
@@ -108,42 +117,35 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 傾印資料表的資料 `user`
+-- 转存表中的数据 `user`
 --
 
 INSERT INTO `user` (`user_id`, `password`, `person_name`, `birthday`, `gender`, `email`, `phone_number`) VALUES
-('asdfads', '11111111qQ', 'asdf', '2023-04-05', 'Male', 'asdf@dasf.com', '2123'),
-('fffff', '11111aA', 'dsf', '2023-04-01', 'Female', 'adsf@dsf.com', '2222'),
-('ffffffff', 'aA11111', 'adfas', '2023-04-05', 'Male', 'adsf@dasf.com', '4444'),
-('gggggg', '12345678Aa', 'asdfadsf', '2023-04-05', 'Male', 'asdf@asdf.com', 'a12121'),
-('ggwp', 'aA12345', 'adsf', '2023-04-04', 'Male', 'adsf@df.com', '333333');
+('aaaaA1', '$2y$10$nE.OSVXL2jXIJcnEl5u4s.BwMk2F6R5LwryxgxxgPGoNLkOGkET62', 'sdf', '2023-04-06', 'Male', 'dsfa@asdf.com', '112121'),
+('ffff', '$2y$10$EX8wU5WJj/g91RKClxag6OrNnUqi2ljsS8KA1pj/4wNEnyonw2B1C', 'jk', '2023-04-04', 'Male', 'asdf@afa.com', '8888888'),
+('ggwp12345', '$2y$10$OEXKTSpxMHBKeG1n1QO1SOia.dUMX50QJidfoZZ9Rm8kNIwAVBGru', 'asdf', '2023-03-27', 'Male', 'sadf@sdaf.com', '1111'),
+('jjjjJ1', '$2y$10$r4Q90E95WfXKmd9ck7oiE.RYjBEo6M/E22Ed7/dppm9ckWXbSPIbq', 'sadfa', '2023-03-29', 'Male', 'sdf@asdf.com', '412312'),
+('submitA1', '$2y$10$i0IeoaLrBhh8UQUf.O4LUeFxs5sHGaJC6Lyz3K3Q/UHh3NOtlHII2', 'iiiii', '2023-04-20', 'Female', 'fffff@dsaf.com', '00000000');
 
 --
--- 已傾印資料表的索引
+-- 转储表的索引
 --
 
 --
--- 資料表索引 `feedback`
+-- 表的索引 `feedback`
 --
 ALTER TABLE `feedback`
   ADD PRIMARY KEY (`feedback_id`);
 
 --
--- 資料表索引 `product`
---
-ALTER TABLE `product`
-  ADD PRIMARY KEY (`product_id`),
-  ADD UNIQUE KEY `product_id` (`product_id`);
-
---
--- 資料表索引 `transaction`
+-- 表的索引 `transaction`
 --
 ALTER TABLE `transaction`
   ADD PRIMARY KEY (`transaction_id`,`user_id`,`product_id`),
   ADD UNIQUE KEY `transaction_id` (`transaction_id`);
 
 --
--- 資料表索引 `user`
+-- 表的索引 `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`),
